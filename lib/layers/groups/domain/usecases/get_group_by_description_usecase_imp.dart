e2aa92f7b329505728d97d31329a5e44c9fb614f
thgoas <thgas95@gmail.com>
2024-06-22 17:08:39 -0300

    feat: Should return a group when the desc and return failure whe there is a failure

---
+++ lib/layers/groups/domain/usecases/get_group_by_description_usecase_imp.dart
@@ -0,0 +1,18 @@
+import 'package:dartz/dartz.dart';
+import 'package:my_finances/layers/groups/domain/entities/group_entity.dart';
+import 'package:my_finances/layers/groups/domain/errors/failure_group.dart';
+import 'package:my_finances/layers/groups/domain/repositories/get_groups_by_description_repository.dart';
+import 'package:my_finances/layers/groups/domain/usecases/get_group_by_description_usecase.dart';
+
+class GetGroupByDescriptionUseCaseImp implements GetGroupByDescriptionUseCase {
+  final GetGroupsByDescriptionRepository _repository;
+
+  GetGroupByDescriptionUseCaseImp(this._repository);
+  @override
+  Future<Either<FailureGroup, GroupEntity>> call(String description) async {
+    if (description.isEmpty) {
+      return Left(InvalidIdError('Invalid id'));
+    }
+    return _repository(description);
+  }
+}
