81fff68ebf2780a618f963ceaf08dbf1aff2545d
thgoas <thgas95@gmail.com>
2024-06-25 22:34:31 -0300

    test: create Update group test

---
+++ test/layers/groups/domain/usecases/update_group_usecase_test.dart
@@ -0,0 +1,66 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:my_finances/layers/groups/domain/entities/group_entity.dart';
+import 'package:my_finances/layers/groups/domain/errors/failure_group.dart';
+import 'package:my_finances/layers/groups/domain/repositories/update_group_repository.dart';
+import 'package:my_finances/layers/groups/domain/usecases/update_group_usecase.dart';
+import 'package:my_finances/layers/groups/domain/usecases/update_group_usecase_imp.dart';
+
+class UpdateGroupRepositoryMock extends Mock implements UpdateGroupRepository {}
+
+main() {
+  late UpdateGroupRepositoryMock repository;
+  late UpdateGroupUseCase usecase;
+  final testGroup = GroupEntity(
+      id: '1',
+      description: 'desc edit',
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now());
+
+  setUpAll(() {
+    // Registre o comportamento esperado para argumentos não nulos
+    registerFallbackValue('');
+  });
+  setUp(() {
+    repository = UpdateGroupRepositoryMock();
+    usecase = UpdateGroupUseCaseImp(repository);
+  });
+
+  group('Update group use case', () {
+    test('should return group edited with success', () async {
+      final id = '1';
+      final groupEdit =
+          GroupEntityInput(description: 'desc edit', updatedAt: DateTime.now());
+      when(() => repository(id, groupEdit))
+          .thenAnswer((_) async => Right(testGroup));
+      final result = await usecase(id, groupEdit);
+
+      expect(result, isA<Right>());
+      expect(result, Right(testGroup));
+    });
+    test('should return exception with id is empty', () async {
+      final idInput = '';
+      final groupEdit =
+          GroupEntityInput(description: 'desc edit', updatedAt: DateTime.now());
+      when(() => repository(idInput, groupEdit))
+          .thenAnswer((_) async => Right(testGroup));
+      final result = await usecase(idInput, groupEdit);
+
+      expect(result, isA<Left>());
+      expect(result.fold(id, id), isA<InvalidIdError>());
+    });
+
+    test('should return exception with description is empty', () async {
+      final idInput = '1';
+      final groupEdit =
+          GroupEntityInput(description: '', updatedAt: DateTime.now());
+      when(() => repository(idInput, groupEdit))
+          .thenAnswer((_) async => Right(testGroup));
+      final result = await usecase(idInput, groupEdit);
+
+      expect(result, isA<Left>());
+      expect(result.fold(id, id), isA<InvalidDescriptionError>());
+    });
+  });
+}
