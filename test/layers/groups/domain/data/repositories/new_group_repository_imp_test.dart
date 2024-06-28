80956f20c3e883e6cdbd7737c28c9e28c92ac1ff
thgoas <thgas95@gmail.com>
2024-06-27 22:24:58 -0300

    test: refactor new group repository

---
+++ test/layers/groups/domain/data/repositories/new_group_repository_imp_test.dart
@@ -0,0 +1,31 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:my_finances/layers/groups/data/repositories/new_group_repository_imp.dart';
+import 'package:my_finances/layers/groups/domain/entities/group_entity.dart';
+
+main() {
+  // late GetOneGroupDataSourceMock getOneGroupDataSource;
+  late NewGroupRepositoryImp repository;
+  // final resultGroup = GroupDto(id: '1', description: 'desc', type: 'type');
+  setUpAll(() {
+    // Registre o comportamento esperado para argumentos não nulos
+    registerFallbackValue('');
+  });
+  setUp(() {
+    // getOneGroupDataSource = GetOneGroupDataSourceMock();
+    repository = NewGroupRepositoryImp();
+  });
+  group('New group repository', () {
+    test('should return a new group', () async {
+      final newgroup = GroupEntity(
+          id: '1',
+          description: 'desc',
+          createdAt: DateTime.now(),
+          updatedAt: DateTime.now());
+      final result = await repository(newgroup);
+      expect(result, isA<Right>());
+      expect(result, Right(newgroup));
+    });
+  });
+}
