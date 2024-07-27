52b89be61f0bc4fffde4e93ff157b80c6d35a923
thgoas <thgas95@gmail.com>
2024-07-27 18:32:43 -0300

    refactor: change entry metodo new group

--- lib/layers/groups/domain/usecases/new_group_usecase.dart
+++ lib/layers/groups/domain/usecases/new_group_usecase.dart
@@ -3,5 +3,5 @@ import 'package:my_finances/layers/groups/domain/entities/group_entity.dart';
 import 'package:my_finances/layers/groups/domain/errors/failure_group.dart';
 
 abstract class NewGroupUseCase {
-  Future<Either<FailureGroup, GroupEntity>> call(GroupEntity groupEntity);
+  Future<Either<FailureGroup, GroupEntity>> call(String description);
 }
--- lib/layers/groups/domain/usecases/new_group_usecase_imp.dart
+++ lib/layers/groups/domain/usecases/new_group_usecase_imp.dart
@@ -3,6 +3,7 @@ import 'package:my_finances/layers/groups/domain/entities/group_entity.dart';
 import 'package:my_finances/layers/groups/domain/errors/failure_group.dart';
 import 'package:my_finances/layers/groups/domain/repositories/group_repository.dart';
 import 'package:my_finances/layers/groups/domain/usecases/new_group_usecase.dart';
+import 'package:uuid/uuid.dart';
 
 class NewGroupUseCaseImp implements NewGroupUseCase {
   final GroupRepository _repository;
@@ -10,11 +11,18 @@ class NewGroupUseCaseImp implements NewGroupUseCase {
   NewGroupUseCaseImp(this._repository);
 
   @override
-  Future<Either<FailureGroup, GroupEntity>> call(
-      GroupEntity groupEntity) async {
-    if (groupEntity.id.isEmpty || groupEntity.description.isEmpty) {
-      return Left(InvalidFieldsError('id or description can not Empty'));
+  Future<Either<FailureGroup, GroupEntity>> call(String description) async {
+    if (description.isEmpty) {
+      return Left(InvalidFieldsError('description can not Empty'));
     }
-    return _repository.save(groupEntity);
+    const groupId = Uuid();
+    final group = GroupEntity(
+        id: groupId.v4(),
+        description: description,
+        createdAt: DateTime.now(),
+        updatedAt: null);
+
+    final result = await _repository.save(group);
+    return result;
   }
 }
