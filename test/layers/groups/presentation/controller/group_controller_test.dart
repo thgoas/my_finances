07e749eb2018cf6cad8a8b82030e75f65d81036e
thgoas <thgas95@gmail.com>
2024-07-28 13:30:33 -0300

    test: create group controller tests

--- test/layers/groups/presentation/controller/group_controller_test.dart
+++ test/layers/groups/presentation/controller/group_controller_test.dart
@@ -6,20 +6,131 @@ import 'package:my_finances/layers/groups/presentation/states/group_state.dart';
 
 void main() {
   Inject.init();
+  final controller = GetIt.instance.get<GroupController>();
   test('should return GroupPageSuccessfulState with id 1', () async {
-    final controller = GetIt.instance.get<GroupController>();
-
     await controller.getGroupById('1');
     expect(controller.state.value, isA<GroupPageSuccessfulState>());
 
     final state = controller.state.value as GroupPageSuccessfulState;
-    expect(state.groupEntity.id, '1');
+    expect(state.groups[0].id, '1');
+  });
+  test('should return GroupPageErrorState when id empty', () async {
+    await controller.getGroupById('');
+    expect(controller.state.value, isA<GroupPageErrorState>());
+
+    final state = controller.state.value as GroupPageErrorState;
+    expect(state.errorMessage, 'Invalid id');
   });
 
-  // test('should return a group id with 1', () async {
-  //   final controller = GetIt.instance.get<GroupController>();
+  test('should return GroupPageErrorState when id not exist', () async {
+    await controller.getGroupById('100');
+    expect(controller.state.value, isA<GroupPageErrorState>());
 
-  //   await controller.getOneGroup('4');
-  //   expect(controller.state.value, isA<GroupPageErrorState>());
-  // });
+    final state = controller.state.value as GroupPageErrorState;
+    expect(state.errorMessage, 'No data found');
+  });
+
+  test('should return GroupPageSuccessfulState with description group 1',
+      () async {
+    await controller.getGroupByDescription('group 1');
+    expect(controller.state.value, isA<GroupPageSuccessfulState>());
+
+    final state = controller.state.value as GroupPageSuccessfulState;
+    expect(state.groups[0].description, 'group 1');
+  });
+
+  test('should return GroupPageErrorState when description empty', () async {
+    await controller.getGroupByDescription('');
+    expect(controller.state.value, isA<GroupPageErrorState>());
+
+    final state = controller.state.value as GroupPageErrorState;
+    expect(state.errorMessage, 'Invalid Description');
+  });
+
+  test('should return GroupPageErrorState when description not exist',
+      () async {
+    await controller.getGroupByDescription('note found');
+    expect(controller.state.value, isA<GroupPageErrorState>());
+
+    final state = controller.state.value as GroupPageErrorState;
+    expect(state.errorMessage, 'No data found');
+  });
+
+  test('should return GroupPageSuccessfulState with all groups', () async {
+    await controller.getAllGroup();
+    expect(controller.state.value, isA<GroupPageSuccessfulState>());
+
+    final state = controller.state.value as GroupPageSuccessfulState;
+    expect(state.groups.length, 3);
+  });
+
+  test('should create a new group', () async {
+    await controller.newGroup('new group');
+    expect(controller.state.value, isA<GroupPageSuccessfulState>());
+
+    final state = controller.state.value as GroupPageSuccessfulState;
+    expect(state.groups[0].description, 'new group');
+  });
+
+  test('should return GroupPageErrorState when create a new group empty',
+      () async {
+    await controller.newGroup('');
+    expect(controller.state.value, isA<GroupPageErrorState>());
+
+    final state = controller.state.value as GroupPageErrorState;
+    expect(state.errorMessage, 'description can not Empty');
+  });
+
+  test('should update group with id 1', () async {
+    await controller.updateGroup('1', 'group 1 edited');
+    expect(controller.state.value, isA<GroupPageSuccessfulState>());
+
+    final state = controller.state.value as GroupPageSuccessfulState;
+    expect(state.groups[0].description, 'group 1 edited');
+  });
+
+  test('should return GroupPageErrorState when update group id empty',
+      () async {
+    await controller.updateGroup('', 'edited group 1');
+    expect(controller.state.value, isA<GroupPageErrorState>());
+    final state = controller.state.value as GroupPageErrorState;
+    expect(state.errorMessage, 'Id can not empty');
+  });
+  test('should return GroupPageErrorState when update group id not exist',
+      () async {
+    await controller.updateGroup('100', 'edited group 100');
+    expect(controller.state.value, isA<GroupPageErrorState>());
+    final state = controller.state.value as GroupPageErrorState;
+    expect(state.errorMessage, 'No data found');
+  });
+
+  test('should return GroupPageErrorState when update group description empty',
+      () async {
+    await controller.updateGroup('1', '');
+    expect(controller.state.value, isA<GroupPageErrorState>());
+    final state = controller.state.value as GroupPageErrorState;
+    expect(state.errorMessage, 'description can not empty');
+  });
+
+  test('should delete group with id 1', () async {
+    await controller.removeGroup('1');
+    expect(controller.state.value, isA<GroupPageSuccessfulState>());
+
+    final state = controller.state.value as GroupPageSuccessfulState;
+    expect(state.groups[0].description, 'group 1 edited');
+  });
+
+  test('should return GroupPageErrorState when id empty', () async {
+    await controller.removeGroup('');
+    expect(controller.state.value, isA<GroupPageErrorState>());
+    final state = controller.state.value as GroupPageErrorState;
+    expect(state.errorMessage, 'Id can not empty');
+  });
+
+  test('should return GroupPageErrorState when id not exits', () async {
+    await controller.removeGroup('100');
+    expect(controller.state.value, isA<GroupPageErrorState>());
+    final state = controller.state.value as GroupPageErrorState;
+    expect(state.errorMessage, 'No data found');
+  });
 }
